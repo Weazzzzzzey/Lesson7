8b04f94ec75f6bdf93339ea8387cc1a2cf1bf52d
Weazzzzzzey <heronightmares@gmail.com>
2020-01-17 18:16:44 +0200

    Pridetas cesarui sriftas

---
+++ DienosDarbasSuChar/DienosDarbasSuChar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DienosDarbasSuChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = 'a';
+            int i = (int)c;
+            Console.WriteLine("i - {0}",i);
+            i++;
+
+            c = (char)i;
+
+            Console.WriteLine("c - {0}",c);
+            Console.ReadLine();
+
+        }
+    }
+}
